4028dde60bb30620d02883278096cb49ac98acc2
AISHWARYA SANTOSH SHINDE <aishwaryash880@gmail.com>
2025-11-04 13:43:31 +0530

    Add files via upload

---
+++ javabasic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class javabasic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       int a = scanner.nextInt();
+       int b = scanner.nextInt();
+         int sum = a + b;
+            System.out.println("The sum is: " + sum);
+    }
+}
